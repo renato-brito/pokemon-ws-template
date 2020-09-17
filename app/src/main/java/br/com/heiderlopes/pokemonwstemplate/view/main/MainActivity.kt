a1e714d8e28ae564a1e42289d1712b583062030e
Heider Lopes <heidertreinamentos@gmail.com>
2020-09-17 16:52:46 -0300

    Inclusao do template do projeto

---
+++ app/src/main/java/br/com/heiderlopes/pokemonwstemplate/view/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package br.com.heiderlopes.pokemonwstemplate.view.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.heiderlopes.pokemonwstemplate.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
